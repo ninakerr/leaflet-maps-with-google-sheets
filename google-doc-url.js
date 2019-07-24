05336145b10e15676b596f81c34112676a165fe2
ninakerr <44279540+ninakerr@users.noreply.github.com>
2019-07-24 15:37:32 -0700

    update doc

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1dT69WN9zPvheEHLCnehTBVY1NnJoYIZF5kWSzHKvqNk/edit#gid=0';
